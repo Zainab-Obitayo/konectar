4ae2b7360808b1ce13df40529fef981c2cf4d8d0
Zainab <zainabobitayo@gmail.com>
2024-10-18 18:28:15 +0100

    new

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 // Import routes
 const userRouter = require('./routes/userRoute');
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to Konectar' });
+  });
+  
+
 // Middleware to parse incoming JSON requests
 app.use(bodyParser.json());
 app.use(express.json());
